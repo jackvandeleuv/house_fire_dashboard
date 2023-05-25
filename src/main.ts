a7b8ae28b186e20f5215812678df23b6bceb94e6
Jack Vandeleuv <jvandele@andrew.cmu.edu>
2023-05-25 19:58:11 -0400

    changed path

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ function handleValue(value: any) {
 }
 
 // Fetch data and add it to the map
-fetch('/house_fire_dashboard/dashboard/dashboard.json')
+fetch('/dashboard/dashboard.json')
     .then(response => response.json())
     .then(data => {
         for (let item of data) {
