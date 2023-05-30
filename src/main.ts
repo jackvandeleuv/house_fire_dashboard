441f64a2118cf0a61b38878aba206273f6f79366
Jack Vandeleuv <jvandele@andrew.cmu.edu>
2023-05-30 19:55:57 -0400

    feat: added percentile rankings for each datapoint

--- src/main.ts
+++ src/main.ts
@@ -16,19 +16,9 @@ interface City {
     AVG_INJURIES: number;
     AVG_ALARMS: number;
     SUPPORT: number;
-    SUMLEV: number;
+    CITY: string;
     STATE: string;
-    COUNTY: number;
-    PLACE: number;
-    COUSUB: number;
-    CONCIT: number;
-    PRIMGEO_FLAG: number;
-    FUNCSTAT: string;
-    NAME: string;
-    STNAME: string;
     POPULATION: number;
-    POPESTIMATE2020: number;
-    POPESTIMATE2021: number;
     TYPE: string;
     COUNT_111_ADJ: number;
     COUNT_113_ADJ: number;
@@ -36,8 +26,23 @@ interface City {
     COUNT_151_ADJ: number;
     COUNT_142_ADJ: number;
     TOTAL_INCIDENT_COUNT_ADJ: number;
-    CITY: string;
-  }
+    AVG_SCORE_MULTIFAMILY_PERCENTILE: number;
+    AVG_SCORE_PUBLIC_PERCENTILE: number;
+    AVG_SPREAD_PERCENTILE: number;
+    AVG_MONEY_LOST_PERCENTILE: number;
+    AVG_FATALITIES_PERCENTILE: number;
+    AVG_INJURIES_PERCENTILE: number;
+    AVG_ALARMS_PERCENTILE: number;
+    POPULATION_PERCENTILE: number;
+    SUPPORT_PERCENTILE: number,
+    COUNT_111_ADJ_PERCENTILE: number;
+    COUNT_113_ADJ_PERCENTILE: number;
+    COUNT_131_ADJ_PERCENTILE: number;
+    COUNT_151_ADJ_PERCENTILE: number;
+    COUNT_142_ADJ_PERCENTILE: number;
+    TOTAL_INCIDENT_COUNT_ADJ_PERCENTILE: number;
+}
+  
 
 function renderMap(): L.Map {
     // Initialize the map
@@ -52,32 +57,83 @@ function renderMap(): L.Map {
     return map;
 }
 
-function generateCardHTML(title: string, value: string) {
+function generateCardHTML(title: string, value: string, percentile: string) {
     return `
-        <div class="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white my-3">
-            <div class="px-6 py-4">
-                <div class="font-semibold text-lg mb-2 text-gray-700">${title}</div>
-                <p class="text-gray-600 text-base">${value}</p>
-            </div>
-        </div>`;
+    <div class="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white my-3">
+    <div class="px-6 py-4">
+        <div class="font-semibold text-lg mb-2 text-gray-700">${title}</div>
+    </div>
+    <div class="bg-gray-200 py-4 px-6">
+        <p class="text-gray-700 font-semibold text-base">
+            <span class="text-xl">${value}</span>
+        </p>
+        <p class="text-gray-500 text-sm">Higher than ${percentile}</p>
+    </div>
+</div>
+
+
+
+    `;
 }
 
 function generateCitySummary(city: City): string {
     return [
-        generateCardHTML("Average Multifamily Inspection Score", formatNumbers(city.AVG_SCORE_MULTIFAMILY, S_DECIMALS)),
-        generateCardHTML("Average Public Inspection Score", formatNumbers(city.AVG_SCORE_PUBLIC, S_DECIMALS)),
-        generateCardHTML("Average Buildings/Vehicles/Entities Ignited (per fire)", formatNumbers(city.AVG_SPREAD, L_DECIMALS)),
-        generateCardHTML("Average Fatalities (per fire)", formatNumbers(city.AVG_FATALITIES, L_DECIMALS)),
-        generateCardHTML("Average Injuries (per fire)", formatNumbers(city.AVG_INJURIES, L_DECIMALS)),
-        generateCardHTML("Average Value of Property Lost (per fire)", dollar_formatter.format(city.AVG_MONEY_LOST)),
-        generateCardHTML("Average Alarms Triggered (per fire)", formatNumbers(city.AVG_ALARMS, L_DECIMALS)),
-        generateCardHTML("Total Reported Fires (per capita)", formatNumbers(city.TOTAL_INCIDENT_COUNT_ADJ, L_DECIMALS)),
-        generateCardHTML("Reported Cooking Fires (per capita)", formatNumbers(city.COUNT_113_ADJ, L_DECIMALS)),
-        generateCardHTML("Reported Passenger Vehicle Fires (per capita)", formatNumbers(city.COUNT_131_ADJ, L_DECIMALS)),
-        generateCardHTML("Reported Outside Trash/Rubbish/Waste Fires (per capita)", formatNumbers(city.COUNT_151_ADJ, L_DECIMALS)),
-        generateCardHTML("Reported Brush/Grass Fires (per capita)", formatNumbers(city.COUNT_142_ADJ, L_DECIMALS)),
-        generateCardHTML("Population", city.POPULATION.toString()),
-        generateCardHTML("Total Fires Reported to NFIRS", city.SUPPORT.toString())
+        generateCardHTML("Average Multifamily Inspection Score", 
+            formatNumbers(city.AVG_SCORE_MULTIFAMILY, S_DECIMALS),
+            formatPercentile(city.AVG_SCORE_MULTIFAMILY_PERCENTILE)
+        ),
+        generateCardHTML("Average Public Inspection Score",
+            formatNumbers(city.AVG_SCORE_PUBLIC, S_DECIMALS),
+            formatPercentile(city.AVG_SCORE_PUBLIC_PERCENTILE)
+        ),
+        generateCardHTML("Average Buildings/Vehicles/Etc. Ignited (per fire)",
+            formatNumbers(city.AVG_SPREAD, L_DECIMALS),
+            formatPercentile(city.AVG_SPREAD_PERCENTILE)
+        ),
+        generateCardHTML("Average Fatalities (per fire)",
+            formatNumbers(city.AVG_FATALITIES, L_DECIMALS),
+            formatPercentile(city.AVG_FATALITIES_PERCENTILE)
+        ),
+        generateCardHTML("Average Injuries (per fire)",
+            formatNumbers(city.AVG_INJURIES, L_DECIMALS),
+            formatPercentile(city.AVG_INJURIES_PERCENTILE)
+        ),
+        generateCardHTML("Average Value of Property Lost (per fire)",
+            dollar_formatter.format(city.AVG_MONEY_LOST),
+            formatPercentile(city.AVG_MONEY_LOST_PERCENTILE)
+        ),
+        generateCardHTML("Average Alarms Triggered (per fire)",
+            formatNumbers(city.AVG_ALARMS, L_DECIMALS),
+            formatPercentile(city.AVG_ALARMS_PERCENTILE)
+        ),
+        generateCardHTML("Total Reported Fires (per capita)",
+            formatNumbers(city.TOTAL_INCIDENT_COUNT_ADJ, L_DECIMALS),
+            formatPercentile(city.TOTAL_INCIDENT_COUNT_ADJ_PERCENTILE)
+        ),
+        generateCardHTML("Reported Cooking Fires (per capita)",
+            formatNumbers(city.COUNT_113_ADJ, L_DECIMALS),
+            formatPercentile(city.COUNT_113_ADJ_PERCENTILE)
+        ),
+        generateCardHTML("Reported Passenger Vehicle Fires (per capita)",
+            formatNumbers(city.COUNT_131_ADJ, L_DECIMALS),
+            formatPercentile(city.COUNT_131_ADJ_PERCENTILE)
+        ),
+        generateCardHTML("Reported Outside Trash/Rubbish/Waste Fires (per capita)",
+            formatNumbers(city.COUNT_151_ADJ, L_DECIMALS),
+            formatPercentile(city.COUNT_151_ADJ_PERCENTILE)
+        ),
+        generateCardHTML("Reported Brush/Grass Fires (per capita)",
+            formatNumbers(city.COUNT_142_ADJ, L_DECIMALS),
+            formatPercentile(city.COUNT_142_ADJ_PERCENTILE)
+        ),
+        generateCardHTML("Population",
+            city.POPULATION.toLocaleString(),
+            formatPercentile(city.POPULATION_PERCENTILE)
+        ),
+        generateCardHTML("Total Fires Reported to NFIRS",
+            city.SUPPORT.toLocaleString(),
+            formatPercentile(city.SUPPORT_PERCENTILE)
+        )
     ].join('');
 }
 
@@ -85,6 +141,11 @@ function formatNumbers(value: string | number, round_to: number): string {
     if (value == null) return 'N/A';
     if (typeof value === 'number') return value.toFixed(round_to).toString();
     return value;
+}
+
+function formatPercentile(val: number | null): string {
+    if (val == null) return 'N/A';
+    return (val * 100).toFixed(0).toString() + '%';
 }
 
 const dollar_formatter = new Intl.NumberFormat('en-US', {
@@ -135,7 +196,7 @@ function renderPage(): void {
                 Average Fatalities (per fire): ${formatNumbers(city.AVG_FATALITIES, L_DECIMALS)}
                 <br>
                 Total Reported Fires (per capita): ${formatNumbers(city.TOTAL_INCIDENT_COUNT_ADJ, L_DECIMALS)}
-            `);
+            `, { autoPan: false });
 
             marker.on('mouseover', function () {
                 marker.openPopup();
